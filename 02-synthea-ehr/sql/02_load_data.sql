5f42e08dd2dcab240c625fc6044a6f9379d7806a
ahmad zia <ahmadzianasrat100@gmail.com>
2026-07-16 19:11:19 +0430

    correct 02_load_data.sql file

--- 02-synthea-ehr/sql/02_load_data.sql
+++ 02-synthea-ehr/sql/02_load_data.sql
@@ -1,6 +1,6 @@
 SET search_path TO synthea_ehr;
 
-\copy patients FROM '02-synthea-ehr/data/patients_utf8.csv' WITH (FORMAT csv, HEADER true);
+\copy patients (id, birthdate, deathdate, ssn, drivers, passport, prefix, first_name, last_name, suffix, maiden_name, marital_status, race, ethnicity, gender, birthplace, address, city, state, county, fips, zip, lat, lon, healthcare_expenses, healthcare_coverage, income) FROM '02-synthea-ehr/data/patients.csv' WITH (FORMAT csv, HEADER true);
 \copy encounters FROM '02-synthea-ehr/data/encounters.csv' WITH (FORMAT csv, HEADER true);
 \copy conditions (start_date, stop_date, patient_id, encounter_id, code, description) FROM '02-synthea-ehr/data/conditions.csv' WITH (FORMAT csv, HEADER true);
 \copy medications (start_time, stop_time, patient_id, payer_id, encounter_id, code, description, base_cost, payer_coverage, dispenses, total_cost, reason_code, reason_description) FROM '02-synthea-ehr/data/medications.csv' WITH (FORMAT csv, HEADER true);
